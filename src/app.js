313c5a9118087531d4cfb8d2e8241701479ae419
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-05-18 19:01:46 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const pastes = require("./data/pastes-data");
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
+app.use(express.json());
+
+
 app.use("/pastes/:pasteId", (req, res, next) => {
   const { pasteId } = req.params;
   const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
@@ -14,8 +17,32 @@ app.use("/pastes/:pasteId", (req, res, next) => {
   }
 });
 
-app.use("/pastes", (req, res) => {
+app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
+});
+
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post("/pastes", (req, res, next) => {
+  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
+  if(text){
+  const newPaste = {
+    id: ++lastPasteId, // Increment last ID, then assign as the current ID
+    name,
+    syntax,
+    exposure,
+    expiration,
+    text,
+    user_id,
+  };
+  pastes.push(newPaste);
+  res.status(201).json({ data: newPaste });
+  } else {
+    res.sendStatus(400);
+  }
+
 });
 
 // Not found handler
